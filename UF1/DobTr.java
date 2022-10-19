d5f9c72a91f889b29233f525bd7352e6ddcde352
Young-sergito <113603939+Young-sergito@users.noreply.github.com>
2022-10-19 17:19:15 +0200

    Activities UF1

---
+++ UF1/DobTr.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Daw.InstitutMvm.Pb;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author YoungSxrgioo
+ */
+public class DobTr {
+    private final static int DOB = 2;// the value that multiply to get double
+    private final static int TR = 3;// the value that multiply to get triple
+    private final static String MSG_1 = "input a number:"; // message for user
+    private final static String MSG_2 = "the double is: "; // message for user
+    private final static String MSG_3 = "the triple is: "; // message for user
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); // method that can allow input values to user
+    int num = 0; // declare variable integer
+    System.out.println(MSG_1); // show message for user
+    num = sc.nextInt(); // can allow input vslues to user
+    System.out.println(MSG_2 + " " +num * DOB); // show the double 
+    System.out.println(MSG_3 + " "+num * TR); // show the triple
+    }
+    
+}
---
+++ UF1/ValInter3.java
@@ -0,0 +1,35 @@
+package com.Daw.InstitutMvm.Pb;
+
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author YoungSxrgioo
+ */
+public class ValInter3 {
+    private static final int MAX = 100;// constant range max for append number
+    private static final int MIN = 1;// constant range min for append number
+    private static final int COUNT = 3 ;
+    private static final String MSG_1 = "append a number into 1 to 100:"; // constant message for explained user  
+    private static final String MSG_2 = "the number append into 1 to 100:";// constant message for explained user
+    public static void main(String[] args) {
+        int num = 0;
+        int i = 0;
+        Scanner sc = new Scanner(System.in); // method scanner for the user append numbers
+        do { // loop do... while for validate if number is into 1 - 100 
+            System.out.println(MSG_1);
+            num = sc.nextInt();
+            i++;        
+        }while((num < MIN || num > MAX)&& i <= 3);   // condition for rep loop
+        
+        System.out.println(MSG_2+" " + num); // print message with result
+        
+    }
+    
+    }
